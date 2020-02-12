9004d5bc2951c95b033dc862e8fc9f9c4293f848
Edgar Munoz <munozedg@users.noreply.github.com>
2020-02-11 18:29:40 -0600

    Update localizar puntos en areas.R

--- R localizar puntos en areas/localizar puntos en areas.R
+++ R localizar puntos en areas/localizar puntos en areas.R
@@ -16,11 +16,11 @@ clientes <- read.csv("clientes.csv")
 
 # cual es el sistema de coordenadas [crs] del mapa?
 zonas_crs   <- st_crs(zonas) 
-# poner los puntos (clientes) en el mismo sistema de coordenadas (crs) de los condados (shape)
+# poner los puntos (clientes) en el mismo sistema de coordenadas (crs) de las zonas (shape)
 clientes <- st_as_sf(clientes, coords = c("x_center", "y_center"), crs = zonas_crs)
 
 # la respuesta a "En que zona postal esta ubicado cada cliente? 
-# ... �apa: cual es la poblacion de la zona postal?" 
+# ... ñapa: cual es la poblacion de la zona postal?" 
 # tip: los nombres de los clientes empiezan con la misma letra de la zona
 # (a prosofito! para validar)
 clientes_zona <- st_join(clientes, zonas[c('PO_NAME','ZIP','poblacion')], join = st_intersects)
